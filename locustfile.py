6cabc83518198ff7f2e62cdd4578f2fce3df9826
Bob <491889203@qq.com>
2021-12-09 23:20:27 +0000

    Assessment 1 finished

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,11 @@
 from locust import HttpUser, task
+import time
 
+class LoadGenerator(HttpUser):
 
-class HelloWorldUser(HttpUser):
+    host = "http://172.21.0.1:8090"
 
     @task
-    def hello_world(self):
-        pass
+    def call_benchmarkapp(self):
+        response = self.client.get("/primecheck")
+        print('{}, user {} call benchmarkapp, url: {} , result: {}'.format(time.strftime('%H:%M:%S'), self, response.url, response.text))
